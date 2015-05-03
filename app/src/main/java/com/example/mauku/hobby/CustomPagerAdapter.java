4c7afa274cf29462b5def6189da9d738ca2ade9a
Mike1991 <black.mmms@gmail.com>
2015-05-03 16:46:28 -0500

    viewpager

---
+++ app/src/main/java/com/example/mauku/hobby/CustomPagerAdapter.java
@@ -0,0 +1,53 @@
+package com.example.mauku.hobby;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+/**
+ * Created by mauku on 3/05/15.
+ */
+public class CustomPagerAdapter extends PagerAdapter {
+
+    private Context mContext;
+    private LayoutInflater mLayoutInflater;
+    public int[] mResources = {
+            R.drawable.one,
+            R.drawable.two,
+            R.drawable.three
+    };
+
+    public CustomPagerAdapter(Context context) {
+        mContext = context;
+        mLayoutInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        //Return length array
+        return mResources.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == ((LinearLayout) object);
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        View itemView = mLayoutInflater.inflate(R.layout.pager_item, container, false);
+        ImageView imageView = (ImageView) itemView.findViewById(R.id.imageView);
+        imageView.setImageResource(mResources[position]);
+        container.addView(itemView);
+        return itemView;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((LinearLayout) object);
+    }
+}
