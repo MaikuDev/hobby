6c10b1d7daa32125176c78a1b8316d71e27df6f1
Mike1991 <black.mmms@gmail.com>
2015-05-03 17:44:46 -0500

    add button facebook

--- app/src/main/java/com/example/mauku/hobby/MainActivity.java
+++ app/src/main/java/com/example/mauku/hobby/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.mauku.hobby;
 
 import android.app.Activity;
+import android.graphics.Typeface;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
@@ -16,6 +18,10 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button facebook=(Button) findViewById(R.id.facebook);
+        Typeface type=Typeface.createFromAsset(getAssets(), "fonts/Roboto-Regular.ttf");
+        facebook.setTypeface(type);
 
         mCustomPagerAdapter = new CustomPagerAdapter(this);
         mViewPager = (ViewPager) findViewById(R.id.pager);
