a5baecdb0900d74abcc3eb23a6bca712eca26c7d
Mike1991 <black.mmms@gmail.com>
2015-05-03 18:05:28 -0500

    Add ripple effect

--- app/src/main/java/com/example/mauku/hobby/MainActivity.java
+++ app/src/main/java/com/example/mauku/hobby/MainActivity.java
@@ -5,8 +5,10 @@ import android.graphics.Typeface;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 
 
@@ -22,6 +24,13 @@ public class MainActivity extends Activity {
         Button facebook=(Button) findViewById(R.id.facebook);
         Typeface type=Typeface.createFromAsset(getAssets(), "fonts/Roboto-Regular.ttf");
         facebook.setTypeface(type);
+
+        facebook.setOnClickListener (new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                Log.d("Prueba", "facebook");
+            }
+        });
 
         mCustomPagerAdapter = new CustomPagerAdapter(this);
         mViewPager = (ViewPager) findViewById(R.id.pager);
